63324672973eb68da6b5e03547551a1e5f62fd54
Jungmyung Kim <jungmyung.kr@gmail.com>
2022-07-17 22:46:49 +0900

    Write, append and read a text file (8-3)

--- Others/Basic-8.py
+++ Others/Basic-8.py
@@ -6,6 +6,8 @@ https://www.youtube.com/watch?v=kWiCuklohdY&t=3s
 
 2022-07-15 02:58:59 ~ 03:10:11
 2022-07-16 03:10:12 ~ 03:17:44
+2022-07-17 03:17:45 ~ 03:26:26
+2022-07:18 03:26:27 ~
 """
 
 # 8-1 표준입출력
@@ -86,6 +88,51 @@ print("{0:.2f}".format(5/3))
 
 
 # 8-3 파일입출력
+
+# 쓰기
+score_file = open("score.txt", "w", encoding="utf8")
+print("수학 : 0", file=score_file)
+print("영어 : 50", file=score_file)
+score_file.close()
+
+# 덮어쓰기
+score_file = open("score.txt", "a", encoding="utf8")
+score_file.write("과학 : 80")
+score_file.write("\n코딩 : 100")
+score_file.close()
+
+# 읽기 : 방법 1
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.read())
+score_file.close()
+
+# 읽기 : 방법 2
+# 줄 별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+score_file = open("score.txt", "r", encoding="utf8")
+print(score_file.readline(), end="")  # end="" 없으면 문장간 줄 바꿈 존재
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+print(score_file.readline(), end="")
+score_file.close()
+
+# 읽기 : 방법 3
+# while 반복문 사용 : file이 총 몇 문장인지 알 수 없을 경우 반복문 사용해 불러오기도 가능
+score_file = open("score.txt", "r", encoding="utf8")
+while True:
+    line = score_file.readline()
+    if not line:  # 더 이상 읽을 문장이 없으면,
+        break  # 반복문 탈출
+    print(line, end="")
+score_file.close()
+
+# 읽기 : 방법 4
+# 모든 문장을 list 형태로 저장
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines()  # list 형태로 저장
+for line in lines:
+    print(line, end="")
+score_file.close()
+
 # 8-4 pickle
 # 8-5 with
 # 8-6 퀴즈 #7

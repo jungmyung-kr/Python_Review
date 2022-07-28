0b2acd205ab1f669c9218779fe320165b4e26275
Jungmyung Kim <jungmyung.kr@gmail.com>
2022-07-28 17:44:29 +0900

    matplotlib set

---
+++ Others/DataAnalysis_and_Visualization/2_Matplotlib/01_graph.py
@@ -0,0 +1,15 @@
+"""
+https://www.youtube.com/watch?v=PjhlUzp_cU0&t=960s
+
+나도코딩
+파이썬 코딩 무료 강의 (활용편5) - 데이터 분석 및 시각화, 이 영상 하나로 끝내세요
+
+2022-07-28 02:55:33 ~ 03:05:22
+"""
+
+'''
+Matplotlib
+: 다양한 형태의 그래프를 통해서 데이터 시각화를 할 수 있는 라이브러리
+'''
+
+import matplotlib as plt
